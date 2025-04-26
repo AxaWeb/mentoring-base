865a18cbb855cd44d8ea69c328678d50ef238a1d
Axaweb <axaweb@mail.ru>
2025-04-26 21:12:50 +0300

    6. JavaScript, TypeScript. Массивы, функции, циклы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,44 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+
+const myCompanyFunc = (i:string) => i;
+const myCompany:string = 'О компании';
+const FincInit = myCompanyFunc(myCompany);
+
+const newPages:Array<number> = [5, 4, 3, 2, 1]
+
+const toUpperCase = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
-  title = 'mentoring-first-project';
+  title = 'mentoring-base';
 
-  headerTop1Item1 = "Главная";
-  headerTop1Item2 = "О компании";
-  headerTop1Item3 = "Каталог";
+  readonly headerTop1Item1 = "Главная";
+  readonly headerTop1Item2 = "О компании";
+  readonly headerTop1Item3 = "Каталог";
+  isheaderTop1Item3 = true;
 
-  headerTop2Item1 = "Каталог";
-  headerTop2Item2 = "Стройматериалы";
-  headerTop2Item3 = "Инструменты";
-  headerTop2Item4 = "Электрика";
-  headerTop2Item5 = "Интерьер и одежда";
+  readonly aboutCompany = myCompany;
+
+  isManHidden:boolean = true;
+
+  readonly newPages = newPages;
+
+  toUpperCase:Array<string> = toUpperCase;
+  isUpperCase:boolean = true;
+  changeMenuText() {
+    this.toUpperCase = toUpperCase.map(
+      item => this.isUpperCase ? item.toUpperCase() : item.toLowerCase()
+    )
+    this.isUpperCase = !this.isUpperCase;
+  }
 }
