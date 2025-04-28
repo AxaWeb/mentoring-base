cc27e07d421cf7578b2f758a124a7e2f4a8c7ca1
Axaweb <axaweb@mail.ru>
2025-04-28 20:09:29 +0300

    7.-Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,15 @@
-import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-
-
-const myCompanyFunc = (i:string) => i;
-const myCompany:string = 'О компании';
-const FincInit = myCompanyFunc(myCompany);
-
-const newPages:Array<number> = [5, 4, 3, 2, 1]
-
-const toUpperCase = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
+import { RouterOutlet } from "@angular/router";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-
 export class AppComponent {
   title = 'mentoring-base';
-
-  readonly headerTop1Item1 = "Главная";
-  readonly headerTop1Item2 = "О компании";
-  readonly headerTop1Item3 = "Каталог";
-  isheaderTop1Item3 = true;
-
-  readonly aboutCompany = myCompany;
-
-  isManHidden:boolean = true;
-
-  readonly newPages = newPages;
-
-  toUpperCase:Array<string> = toUpperCase;
-  isUpperCase:boolean = true;
-  changeMenuText() {
-    this.toUpperCase = toUpperCase.map(
-      item => this.isUpperCase ? item.toUpperCase() : item.toLowerCase()
-    )
-    this.isUpperCase = !this.isUpperCase;
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {UsersListComponent} from "./users-list/users-list.component";
+import {MainPageComponent} from "./main-page/main-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UsersListComponent },
+  { path: '', component: MainPageComponent}
+];
