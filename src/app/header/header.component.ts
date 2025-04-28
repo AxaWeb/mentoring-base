fa5451715a5f3abc5396833a46adc2dd49300e3f
Axaweb <axaweb@mail.ru>
2025-04-28 20:25:58 +0300

    7.-Angular

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {NgFor, NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
+
+const myCompanyFunc = (i:string) => i;
+const myCompany:string = 'О компании';
+const FincInit = myCompanyFunc(myCompany);
+
+const toUpperCase = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NgIf, NgFor, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  readonly headerTop1Item1 = "Главная";
+  readonly headerTop1Item3 = "Каталог";
+  isheaderTop1Item3 = true;
+
+  readonly aboutCompany = FincInit;
+
+  toUpperCase:Array<string> = toUpperCase;
+  isUpperCase:boolean = true;
+  changeMenuText() {
+    this.toUpperCase = toUpperCase.map(
+      item => this.isUpperCase ? item.toUpperCase() : item.toLowerCase()
+    )
+    this.isUpperCase = !this.isUpperCase;
+  }
+}
