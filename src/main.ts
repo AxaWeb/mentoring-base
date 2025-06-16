c95279ce3291b29c6d42437a0f41fce30619ac26
Axaweb <axaweb@mail.ru>
2025-06-16 20:55:16 +0500

    14. Pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+import { LOCALE_ID } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localeRu);
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    { provide: LOCALE_ID, useValue: 'ru' }
+  ]
+}).catch((err) => console.error(err));
